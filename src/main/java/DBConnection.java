114db1a2540944bc8614a476836a7c798c83583e
kajianaLeVrai999 <hei.kajiana@gmail.com>
2026-01-29 14:56:35 +0300

    Fix DBConnection configuration for mini_dish_db

--- src/main/java/DBConnection.java
+++ src/main/java/DBConnection.java
@@ -6,10 +6,7 @@ public class DBConnection {
 
     public Connection getConnection() {
         try {
-            String jdbcURl = System.getenv("JDBC_URl"); //
-            String user = System.getenv("USER"); //mini_dish_db_manager
-            String password = System.getenv("PASSWORD"); //123456
-            return DriverManager.getConnection("jdbc:postgresql://localhost:5432/mini_dish_db", "postgres", "postgres");
+            return DriverManager.getConnection("jdbc:postgresql://localhost:5432/mini_dish_db", "mini_dish_db_manager", "123456");
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
